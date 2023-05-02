0e5f240dedd5c6948f4cceb86882b771163da0b4
André Eriksson <andretech1@hotmail.com>
2023-05-02 20:04:03 +0200

    First commit
    Added Docker and PostgreSQL to the Spring Boot application.
    
    This application is for planning trips across the world using APIs from other sources such as Google and Booking etc. Also is going to support logins, authenticantions, and a front-end (Later)

---
+++ src/main/java/com/beastlymc/triptimize/TriptimizeApplication.java
@@ -0,0 +1,17 @@
+package com.beastlymc.triptimize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TriptimizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(TriptimizeApplication.class);
+
+        application.run(args);
+    }
+
+}
---
+++ src/test/java/com/beastlymc/triptimize/TriptimizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beastlymc.triptimize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptimizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
