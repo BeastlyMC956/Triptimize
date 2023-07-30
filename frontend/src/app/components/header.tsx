68658e0d82c3a8d43cc91196c0989e81bb0d3f03
André Eriksson <andretech1@hotmail.com>
2023-07-30 16:15:31 +0200

    Responsive Header #1
    Created a responsive header for mobile/smaller screens only. Still need to create the actual header for bigger screens. The color schema is temporary and will undoubtably change later on.

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -1,44 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
-import Icon from '../../../public/white_icon.png'
-import Menu from '../../../public/menu_icon.png'
 import styles from './header.module.css';
-
-const linkStyle = 'h-full flex items-center hover:border-b-black hover:border-b-2';
-var menuStyle = 'w-16 h-16 absolute cursor-pointer'
-
-const menuSlide = (): React.MouseEventHandler<HTMLDivElement> | undefined => {
-    menuStyle = ''
-
-    return undefined;
-}
+import Menu from './menu'
 
 const Header = () => {
-    return (
-        <header>
-            <nav className='h-16 flex justify-between'>
-                <div className={`${styles.container} ${styles.smallMenu} w-16 h-16 absolute cursor-pointer`} onClick={menuSlide()}>
-                    <img src={Menu.src} />
-                </div>
-                <div className={`${styles.smallContainer} w-1/3 flex justify-evenly`}>
-                    <div className='w-28 flex items-center'> 
-                        <Link href="/" className='logo h-full flex items-center'>
-                            <img src={Icon.src} />
-                        </Link>
-                    </div>
-                    <div className='w-3/5 h-full flex items-center justify-evenly'>
-                        <Link href="/" className={linkStyle}>Home</Link>
-                        <Link href="/about" className={linkStyle}>Travel</Link>
-                        <Link href="/contact" className={linkStyle}>Contact</Link>
-                        <Link href="/blog" className={linkStyle}>Blog</Link>
-                    </div>
-                </div>
-                <div className={`${styles.smallContainer} w-1/4 flex items-center justify-around`}>
-                    <Link href="/signup" className={linkStyle}>Sign-Up</Link>
-                </div>
-                
-            </nav>
-        </header>
+  
+  return (
+    <header className={`${styles.header} h-16`}>
+      {/* Smaller Screens */}
+      <Menu />
+
+      {/* Wider Screen */}
+      <nav className='wideNav'>
+
+      </nav>
+
+    </header>
     )
 }
 
---
+++ frontend/src/app/components/menu.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useState } from 'react'
+import Link from 'next/link';
+import styles from './menu.module.css';
+import Icon from '../../../public/dark_icon.png'
+import MenuIcon from '../../../public/menu_icon.png'
+
+const linkStyle = 'flex justify-center items-center w-full h-full border-b-4 border-zinc-800';
+
+const Menu = () => {
+  const [showMenu , setShowMenu ] = useState(true);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+  return (
+    <div>
+      <button type='button' onClick={handleMenu} className='w-16 h-16 absolute cursor-pointer'>
+        <img src={MenuIcon.src} />
+      </button>
+
+      <div className={`${styles.slidingMenu} flex flex-col`} style={{ transform: !showMenu ? 'translateX(0)' : '' }}>
+        <div className='w-full flex justify-center'>
+          <img src={Icon.src} />
+        </div>
+        
+        <nav className='w-full h-5/6 flex flex-col justify-around'>
+          <Link href='/' className={linkStyle}>Home</Link>
+          <Link href='/' className={linkStyle}>Travel</Link>
+          <Link href='/' className={linkStyle}>Contact</Link>
+          <Link href='/' className={linkStyle}>Blog</Link>
+        </nav>
+
+        <div className='w-full flex justify-center h-1/6 bg-zinc-800'>
+          <button onClick={handleMenu} className='h-full w-full'>CLOSE</button>
+        </div>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Menu;
